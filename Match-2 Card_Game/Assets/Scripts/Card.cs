10d33cbe9310c2d1485da622275a400f3a357eba
KhizarFarooqGameDev <juniorhsien@gmail.com>
2022-02-23 05:05:56 +0500

    added card flip logic

--- Match-2 Card_Game/Assets/Scripts/Card.cs
+++ Match-2 Card_Game/Assets/Scripts/Card.cs
@@ -1,16 +1,46 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-   [SerializeField] private SpriteRenderer cardSprites;
+   [SerializeField] private SpriteRenderer cardFrontSprite;
+   [SerializeField] private GameObject cardBackSprite;
    private bool isTouchingCard;
+   private bool isFlipped;
+   float x = 0;
+   
+   private void Update()
+   {
+      if (isTouchingCard)
+      {
+         if (Input.GetKeyDown(KeyCode.Space) && !isFlipped)
+         {
+            Flip();
+            isFlipped = true;
+         }
+      }
+   }
+
+   void Flip()
+   {
+      transform.DORotate(new Vector3(0f, 0f, 180f), 1f).OnUpdate(() =>
+      {
+         x += Time.deltaTime;
+
+         if (x > 0.49f)
+         {
+            cardFrontSprite.gameObject.SetActive(false);
+            cardBackSprite.SetActive(true);
+         }
+      });
+   }
 
    public void ChangeCardAlpha(float newValue)
    {
-      Color cardColor = cardSprites.color;
+      Color cardColor = cardFrontSprite.color;
       cardColor.a = newValue;
-      cardSprites.color = cardColor;
+      cardFrontSprite.color = cardColor;
    }
    
    private void OnTriggerEnter(Collider other)
